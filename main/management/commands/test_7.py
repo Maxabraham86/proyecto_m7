7e0e6e64035b3f8f45a8572dff42c9957dfda0a1
Maximiliano Abraham <maxabrahamproyectos@gmail.com>
2024-07-10 02:02:19 -0400

    [ADD] Se agregan en services agregar_inmueble, editar_inmueble y eliminar_inmueble, con sus respectivas pruebas en test_7.py, las funciones se ejecutan adecuadamente en la consola y los datos se reflejan fielmente en el pgadmin

--- main/management/commands/test_7.py
+++ main/management/commands/test_7.py
@@ -1,7 +1,7 @@
 
 from django.core.management.base import BaseCommand
 from main.models import UserProfile, Comuna, Inmueble, Solicitud
-from main.services import crear_user
+from main.services import crear_user, editar_user, crear_inmueble, editar_inmueble
 class Command(BaseCommand):
         
 
@@ -10,20 +10,33 @@ class Command(BaseCommand):
         #funciona con la funcion que ya funcionaba 
         #c=crear_user('11111111-1', 'Juan', 'Marino', 'aaa@bbb.ccc', '1234', 'calle falsa 123', '123456789')
         
-        crear_user('1234567-8', 'Pedro', 'Picapiedra', 'aaa@bbb.ccc', '1234', '1234' , 'Av Rocadira 48')
+        #crear_user('1234567-8', 'Pedro', 'Picapiedra', 'aaa@bbb.ccc', '1234', '1234' , 'Av Rocadura 48')
+        #crear_user('111222333-4', 'Sandro', 'de Lucia', 'aaa@bbb.ccc', '1234', '1234' , 'la alhambra 333')
+        #crear_user('112233445-5', 'Octubre', 'Capilla', 'aaa@bbb.ccc', '1234', '1234' , 'el milagro 28')
+        
+        #editar_user('1234567-8', 'PePe', 'Picapiedra', 'ddd@eee.fff', '4321', 'El granito 22')
 
-
-        # inmueble4= Inmueble.objects.create(
-        #     nombre='Bolsos Cerrado',
+    
+        # crear_inmueble(
+        #     nombre='Bolsón Cerrado',
         #     descripcion ='Gran casa en hermosa y apasible villa rural',
         #     M2_construidos= 800,
         #     M2_totales_terreno=1000,
-        #     direccion='ls Comarca 24',
-        #     comuna =comuna4,
-        #     tipo_vivienda = 'par',
+        #     direccion='la Comarca 24',
         #     precio_mensual= 1500000,
+        #     tipo_vivienda = 'par',
         #     cant_estacionamientos =8,
         #     cant_habitacion =12,
-        #     cant_banos=5
+        #     cant_banos=5,
+        #     comuna = 'viña del mar',
+        #     propietario= '111222333-4'
         # )
+
+        #crear_inmueble ('casa de lucia', 'casa en la que vive de lucia', 150,250, 3,3,2,'granada 338', 600000, 'casa','viña del mar', '111222333-4' ) 
+        #crear_inmueble ('depto de Betty', 'departamento de Betty Boop', 80,80, 1,2,2,'granada 338', 200000, 'depto','valdivia', '11111111-1' )
         
+        #crear_inmueble('Casa Grande en Villarrica', 'Esta es la descripcion de propiedad', 150, 250, 3, 5, 4, 'volcan 22', 700000, 'par', 'villarrica', '112233445-5')
+        
+        editar_inmueble(1,'Parcela en Villarrica', 'propiedad ideal para relajarse y cuidar animales', 150, 250, 3, 5, 4, 'volcan 22', 700000, 'par', 'villarrica', '112233445-5')
+        
+        return 'okidoki'
--- main/services.py
+++ main/services.py
@@ -21,19 +21,22 @@ from django.db.utils import IntegrityError
 
 #metodo trabajado en clases
 
-def crear_user(rut: str,first_name:str ,last_name:str, email:str, password:str, pass_confirm:str, direccion:str, telefono:str =None)->bool:
+def crear_user(rut: str,first_name:str ,last_name:str, email:str, password:str, pass_confirm:str, direccion:str, telefono:str =None)->[bool, str]:
         #validamos que el password coincida
     if password !=pass_confirm:
-        return False
+        return False, 'las contraseñas no coinciden'
+    
     #2 creamos objeto user
-    user = User.objects.create_user(
-        username= rut,
-        first_name = first_name,
-        last_name= last_name,
-        email= email,
-        password=  password,
-        
+    try:
+        user = User.objects.create_user(
+            username= rut,
+            first_name = first_name,
+            last_name= last_name,
+            email= email,
+            password=  password,
     )
+    except IntegrityError:
+        return False, 'Rut duplicado'
     #3 creamos el UserProfile
     
     UserProfile.objects.create(
@@ -45,27 +48,75 @@ def crear_user(rut: str,first_name:str ,last_name:str, email:str, password:str, 
     #4 si todo sale bien retornamos True
     return True
 
-def crear_inmueble(nombre, descripcion,direccion, M2_construidos, M2_totales_terreno, tipo_vivienda, precio_mensual,cant_estacionamientos,cant_habitacion,cant_banos, comuna_id, propietario_id ):
-#inmueble1= Inmueble.objects.create( 
-    
-        comuna = Comuna.objects.get(id=comuna_id)
-        proprietario = User.objectos.get (id_propietario)
-        inmueble=Inmueble(
-            nombre= nombre,
-            descripcion =descripcion,
-            M2_construidos= M2_construidos,
-            M2_totales_terreno= M2_totales_terreno,
-            direccion=direccion,
-            comuna =comuna,
-            tipo_vivienda = tipo_vivienda,
-            precio_mensual= precio_mensual,
-            cant_estacionamientos =cant_estacionamientos,
-            cant_habitacion =cant_habitacion,
-            cant_banos=cant_banos
-        )
+def editar_user(username: str,first_name:str ,last_name:str, email:str, password:str, direccion:str, telefono:str =None):
+    user=User.objects.get(username=username)
+    user.first_name = first_name
+    user.last_name = last_name
+    user.email = email
+    user.set_password = (password)
+    user.save()
 
-def editar_inmueble(*args):
-    pass
+    user_profile = UserProfile.objects.get(user=user)
+    user_profile.direccion = direccion
+    user_profile.telefono = telefono
+    user_profile.save()
+
+
+# def crear_inmueble(nombre, descripcion,direccion, M2_construidos, M2_totales_terreno, tipo_vivienda, precio_mensual,cant_estacionamientos,cant_habitacion,cant_banos, comuna_id, propietario_id ):
+# #inmueble1= Inmueble.objects.create( 
+    
+#         comuna = Comuna.objects.get(id=comuna_id)
+#         proprietario = User.objectos.get(id_propietario)
+#         inmueble=Inmueble(
+#             nombre= nombre,
+#             descripcion =descripcion,
+#             M2_construidos= M2_construidos,
+#             M2_totales_terreno= M2_totales_terreno,
+#             direccion=direccion,
+#             comuna =comuna,
+#             tipo_vivienda = tipo_vivienda,
+#             precio_mensual= precio_mensual,
+#             cant_estacionamientos =cant_estacionamientos,
+#             cant_habitacion =cant_habitacion,
+#             cant_banos=cant_banos,
+#             propietario=propietario
+#         )
+
+def crear_inmueble(nombre:str, descripcion:str, M2_construidos:int, M2_totales_terreno:int, cant_estacionamientos:int, cant_habitaciones:int, cant_banos:int, direccion:str, precio_mensual:int, tipo_vivienda:str, comuna:str, rut_propietario:str):
+    comuna = Comuna.objects.get(nombre=comuna)
+    propietario = User.objects.get(username=rut_propietario)
+    Inmueble.objects.create(
+        nombre = nombre,
+        descripcion = descripcion,
+        M2_construidos = M2_construidos,
+        M2_totales_terreno = M2_totales_terreno,
+        cant_estacionamientos = cant_estacionamientos,
+        cant_habitaciones = cant_habitaciones,
+        cant_banos = cant_banos,
+        direccion = direccion,
+        tipo_vivienda = tipo_vivienda,
+        precio_mensual = precio_mensual,
+        comuna = comuna,
+        propietario = propietario
+    )
+
+def editar_inmueble(inmueble_id:int, nombre:str, descripcion:str, M2_construidos:int, M2_totales_terreno:int, cant_estacionamientos:int, cant_habitaciones:int, cant_banos:int, direccion:str, precio_mensual:int, tipo_vivienda:str, comuna:str, rut_propietario:str):
+    inmueble=Inmueble.objects.get(id=inmueble_id)
+    comuna = Comuna.objects.get(nombre=comuna)
+    propietario = User.objects.get(username=rut_propietario)
+    inmueble.nombre = nombre          
+    inmueble.descripcion = descripcion                           
+    inmueble.M2_construidos = M2_construidos
+    inmueble.M2_totales_terreno = M2_totales_terreno
+    inmueble.cant_estacionamientos = cant_estacionamientos
+    inmueble.cant_habitaciones = cant_habitaciones
+    inmueble.cant_banos = cant_banos
+    inmueble.direccion = direccion
+    inmueble.tipo_vivienda = tipo_vivienda
+    inmueble.precio_mensual = precio_mensual
+    inmueble.comuna = comuna
+    inmueble.propietario = propietario
+    inmueble.save()
 
 def eliminar_inmueble(inmueble_id):
     eliminar=Inmueble.objects.get()
@@ -73,8 +124,6 @@ def eliminar_inmueble(inmueble_id):
 
 
 
-def editar_user(*args):
-    pass
 
 def eliminar_user(rut:str):
     eliminar = User.object.get(username=rut)
